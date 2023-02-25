c9bfd4a958668c6bffa26dac92e9b39490e21472
essienricch <essienrichard98@gmail.com>
2023-02-24 16:18:50 -0800

    start-up

---
+++ src/main/java/project/africa/emailing_app/EmailingAppApplication.java
@@ -0,0 +1,13 @@
+package project.africa.emailing_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/africa/emailing_app/EmailingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package project.africa.emailing_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
